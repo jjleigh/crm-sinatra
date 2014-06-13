de2a7a91183213c0831fa04fad7ae71de7829fec
justine jones <justinejones@justines-MacBook-Air.local>
2014-06-13 16:01:46 -0400

    contact fetched

--- crm.rb
+++ crm.rb
@@ -21,7 +21,7 @@ get '/contacts/new' do
 end
 
 get '/contacts/:id' do
-	@contact = @@rolodex.find(params[:id].to_i)
+	@contact = Contact.get(params[:id].to_i)
   if @contact
     erb :show_contact
   else
