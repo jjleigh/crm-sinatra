3276977d51df0e3a2d340b398d27d71f43ac0ece
justine jones <justinejones@justines-MacBook-Air.local>
2014-06-13 11:57:01 -0400

    added the show contacts

--- crm.rb
+++ crm.rb
@@ -1,35 +1,49 @@
 require_relative 'contact'
 require_relative 'rolodex'
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, 'sqlite3:database.sqlite3')
 
 @@rolodex = Rolodex.new
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+
 
 get '/' do 
-	@crm_app_name = "My crm"
+	@crm_app_name = "Justine's crm"
 	erb :index
 end
+
+get '/contacts' do
+	erb :contacts
+end 
 
 get '/contacts/new' do
 	erb :new_contact
 end
 
-get '/contacts' do
-	@contacts = []
-	@contacts << Contact.new(1000, "Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-	erb :contacts
+get '/contacts/:id' do
+	@contact = @@rolodex.find(params[:id].to_i)
+	erb :show_contact
+end
+
+get '/contacts/:id/edit' do
+	@contact = @@rolodex.find(params[:id].to_i)
+	erb :edit_contact
+end 
+
+put '/contacts/:id' do
+	# update an id's stuff
+end
+
+delete '/contacts/:id' do
+  # delete the contact with the specified id 
 end 
 
 post '/contacts' do 
 	contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
 	@@rolodex.add_contact(contact)
-	redirect_to('/contacts')
-end 
-
-get '/contacts/:id' do
-
-end 
-
-get '/contacts/:id/edit' do
+	redirect '/contacts'
 end 
 
 
